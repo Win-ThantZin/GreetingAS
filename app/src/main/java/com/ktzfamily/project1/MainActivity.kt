87763459b2aab1841de2b90d6dabeb2aa2bfabab
KTZ <thntzin@gmail.com>
2024-01-04 14:53:06 +0630

    Initial Commitment!

---
+++ app/src/main/java/com/ktzfamily/project1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ktzfamily.project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
